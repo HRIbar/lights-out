3ba9bda23a6eee717bcffca5b4f6cbdea5daf93e
Klemen Hribar <klemen.hribar@hpe.com>
2023-09-27 11:06:52 +0200

    SolutionsAPI and ProblemsAPI skeletons
    SolutionsAPI and ProblemsAPI controller skeletons added

--- src/main/java/com/example/lightsout/web/ProblemsAPIController.java
+++ src/main/java/com/example/lightsout/web/ProblemsAPIController.java
@@ -1,4 +1,43 @@
 package com.example.lightsout.web;
 
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
+
+@RestController
 public class ProblemsAPIController {
+
+    @GetMapping(value = "/lightsout/problems", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getProblems() throws Exception {
+        String jsonResponse = new JSONObject("{\"message\" : \"This is Lights out problems controller"
+                + " and return all problems!\"}").toString();
+
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+
+    }
+
+    @GetMapping(value = "/lightsout/problems/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getProblemId(@NotBlank @PathVariable String id) throws Exception {
+        String jsonResponse = new JSONObject("{\"message\" : \"This is Lights out problems"
+                + "by Id controller and returns problems with id: " + id + "\"}").toString();
+
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+
+    }
+
+    @PostMapping(value = "/lightsout/problems", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> createProblem() throws Exception {
+        String jsonResponse = new JSONObject("{\"message\" : \"This is Lights out POST problem controller"
+                + " and creates a new problem!\"}").toString();
+
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+
+    }
 }
--- src/main/java/com/example/lightsout/web/SolutionsAPIController.java
+++ src/main/java/com/example/lightsout/web/SolutionsAPIController.java
@@ -5,7 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
 
 @RestController
 public class SolutionsAPIController {
@@ -13,8 +16,19 @@ public class SolutionsAPIController {
     @GetMapping(value = "/lightsout/solutions", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getSolutions() throws Exception {
 
-        String jsonResponse = new JSONObject("{\"message\" : \"This is Lights out solutions controller!\"}").toString();
+        String jsonResponse = new JSONObject("{\"message\" : \"This is Lights out solutions controller"
+                + " and return all optimal solutions!\"}").toString();
 
         return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
     }
+
+    @GetMapping(value = "/lightsout/solutions/problem/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getSolutionById(@NotBlank @PathVariable String id) throws Exception {
+
+        String jsonResponse = new JSONObject("{\"message\" : \"This is Lights out solutions"
+                + "by Id controller and return optimal solutions for solution id: " + id + "\"}").toString();
+
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+    }
+
 }
