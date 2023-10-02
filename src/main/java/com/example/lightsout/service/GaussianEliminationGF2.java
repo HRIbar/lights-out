b8f0fbd982de20b5295a747e9b2d28c8f884bca8
Klemen Hribar <klemen.hribar@hpe.com>
2023-10-02 17:13:51 +0200

    Using GaussianElimination as an optimal algorithm to solve the matrix
    Using GaussianElimination as an optimal algorithm to solve the matrix

---
+++ src/main/java/com/example/lightsout/service/GaussianEliminationGF2.java
@@ -0,0 +1,60 @@
+package com.example.lightsout.service;
+
+public class GaussianEliminationGF2 {
+
+    public static int[] solve(int[][] matrix, int[] vector) {
+        int n = matrix.length;
+        for (int row = 0; row < n; row++) {
+            // Find a row with a nonzero entry and swap
+            int swapRow = row;
+            while (swapRow < n && matrix[swapRow][row] == 0) {
+                swapRow++;
+            }
+            if (swapRow == n) {
+                continue;  // No solution if all zero in this column
+            }
+            swap(matrix, row, swapRow);
+            swap(vector, row, swapRow);
+
+            // Eliminate below
+            for (int i = row + 1; i < n; i++) {
+                if (matrix[i][row] == 1) {
+                    xorRows(matrix, i, row);
+                    vector[i] ^= vector[row];
+                }
+            }
+        }
+
+        // Back substitution
+        int[] solution = new int[n];
+        for (int row = n - 1; row >= 0; row--) {
+            int sum = vector[row];
+            for (int col = row + 1; col < n; col++) {
+                sum ^= (matrix[row][col] * solution[col]);
+            }
+            solution[row] = sum;
+        }
+
+        return solution;
+    }
+
+    private static void swap(int[][] matrix, int row1, int row2) {
+        int[] temp = matrix[row1];
+        matrix[row1] = matrix[row2];
+        matrix[row2] = temp;
+    }
+
+    private static void swap(int[] vector, int row1, int row2) {
+        int temp = vector[row1];
+        vector[row1] = vector[row2];
+        vector[row2] = temp;
+    }
+
+    private static void xorRows(int[][] matrix, int row1, int row2) {
+        int n = matrix[0].length;
+        for (int j = 0; j < n; j++) {
+            matrix[row1][j] ^= matrix[row2][j];
+        }
+    }
+}
+
--- src/main/java/com/example/lightsout/service/LightsOutSolverService.java
+++ src/main/java/com/example/lightsout/service/LightsOutSolverService.java
@@ -7,91 +7,43 @@ import java.util.*;
 @Service
 public class LightsOutSolverService {
 
-    private static final int[] dx = {0, 0, 1, 0, -1};
-    private static final int[] dy = {0, 1, 0, -1, 0};
-
     public int[][] solve(int[][] problem) {
-        Set<String> visited = new HashSet<>();
-        Queue<int[][]> boardQueue = new LinkedList<>();
-        Queue<int[][]> toggleQueue = new LinkedList<>();
         int n = problem.length;
         int m = problem[0].length;
+        int[][] matrix = new int[n * m][n * m];
+        int[] vector = new int[n * m];
 
-        boardQueue.offer(problem);
-        toggleQueue.offer(new int[n][m]);  // Initialize toggle matrix with zeros
-        visited.add(matrixToString(problem));
-
-        while (!boardQueue.isEmpty()) {
-            int[][] currentBoard = boardQueue.poll();
-            int[][] currentToggle = toggleQueue.poll();
-
-            if (isSolved(currentBoard)) {
-                return currentToggle;  // Return the toggle matrix
-            }
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    int[][] nextBoard = toggle(currentBoard, i, j);
-                    int[][] nextToggle = copyMatrix(currentToggle);
-                    nextToggle[i][j] ^= 1;  // Toggle the corresponding field in the toggle matrix
-                    String nextStr = matrixToString(nextBoard);
-                    if (!visited.contains(nextStr)) {
-                        visited.add(nextStr);
-                        boardQueue.offer(nextBoard);
-                        toggleQueue.offer(nextToggle);
+        // Build the matrix and vector for the linear system
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int row = i * m + j;
+                vector[row] = problem[i][j];
+                for (int di = -1; di <= 1; di++) {
+                    for (int dj = -1; dj <= 1; dj++) {
+                        int ni = i + di, nj = j + dj;
+                        if (ni >= 0 && ni < n && nj >= 0 && nj < m && (di == 0 || dj == 0)) {
+                            int col = ni * m + nj;
+                            matrix[row][col] = 1;
+                        }
                     }
                 }
             }
         }
 
-        return new int[0][0];  // Return null if no solution is found
-    }
+        // Solve the system using Gaussian elimination
+        int[] solutionVector = GaussianEliminationGF2.solve(matrix, vector);
 
-    private int[][] toggle(int[][] board, int x, int y) {
-        int n = board.length;
-        int m = board[0].length;
-        int[][] newBoard = new int[n][m];
+        // Convert the solution vector back to a 2D array
+        int[][] solution = new int[n][m];
         for (int i = 0; i < n; i++) {
-            System.arraycopy(board[i], 0, newBoard[i], 0, m);
-        }
-        for (int d = 0; d < 5; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                newBoard[nx][ny] ^= 1;
+            for (int j = 0; j < m; j++) {
+                int row = i * m + j;
+                solution[i][j] = solutionVector[row];
             }
         }
-        return newBoard;
-    }
 
-    private boolean isSolved(int[][] board) {
-        for (int[] row : board) {
-            for (int cell : row) {
-                if (cell == 1) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    private String matrixToString(int[][] matrix) {
-        StringBuilder sb = new StringBuilder();
-        for (int[] row : matrix) {
-            for (int cell : row) {
-                sb.append(cell);
-            }
-        }
-        return sb.toString();
-    }
-    private int[][] copyMatrix(int[][] original) {
-        int n = original.length;
-        int m = original[0].length;
-        int[][] copy = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            System.arraycopy(original[i], 0, copy[i], 0, m);
-        }
-        return copy;
+        return solution;
     }
 }
+
 
